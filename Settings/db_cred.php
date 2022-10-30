87647952ee5f2f253339c8d2e67de787bfc9457a
OgechiNwankwo <95922876+OgechiNwankwo@users.noreply.github.com>
2022-10-30 23:34:32 +0000

    sxs

--- Settings/db_cred.php
+++ Settings/db_cred.php
@@ -1,9 +1,10 @@
 <?php
 
-define("DB_NAME", "shoppn");
-define("DB_SERVER", "localhost");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
+define("DB_NAME", "heroku_3c7a5a960eead28");
+define("DB_SERVER", "us-cdbr-east-06.cleardb.net");
+define("DB_USERNAME", "bd9969aef0f07a");
+define("DB_PASSWORD", "9c2fc5f3");
 
 
 ?>
+
