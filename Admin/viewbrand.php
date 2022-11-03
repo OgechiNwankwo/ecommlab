4f1b72f44cccf7d4b0b09442d40dc7a3ac20a6ce
OgechiNwankwo <95922876+OgechiNwankwo@users.noreply.github.com>
2022-11-03 21:46:41 +0000

    x sd s

--- Admin/viewbrand.php
+++ Admin/viewbrand.php
@@ -1,5 +1,5 @@
 <?php
-    include("../settings/core.php");
+    // include("../settings/core.php");
     include("../controllers/product_controller.php");
 ?>
 
@@ -13,7 +13,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">       
   
         <!-- Bootstrap CSS for styling and layout-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         
 
     </head>
@@ -52,7 +52,7 @@
 								}
 							}
 							displayAllCtr();
-                            
+
 							?>
 						</tbody>
             </table>
