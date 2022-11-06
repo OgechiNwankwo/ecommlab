beec7196ff1191648a4fb171d259fd4d3c0a72a2
OgechiNwankwo <95922876+OgechiNwankwo@users.noreply.github.com>
2022-11-06 23:52:10 +0000

    odne

--- Admin/admin_index.php
+++ Admin/admin_index.php
@@ -5,6 +5,7 @@
 <html>
 <h4><a href="brand.php">Brand</a></h4>
 <h4><a href="category.php">Category</a></h4>
+<h4><a href="product.php">Product</a></h4>
 <h4><a href="../index.php">Home Page</a></h4>
 
     
